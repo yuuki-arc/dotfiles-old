aa8eb1e2469b80160bbf42cefa5b4b781cbb336f
yuuki-arc <arcs7nine@gmail.com>
2015-06-03 14:22:42 +0900

    cygwin用のコマンド文字化け対応

---
+++ home/.zsh.d/.oh-my-zsh-custom/plugins/cygwin/cygwin.plugin.zsh
@@ -0,0 +1,12 @@
+function wincmd() {
+    CMD=$1
+    shift
+    $CMD $* 2>&1 | iconv -f cp932 -t utf-8
+}
+ 
+alias ipconfig='wincmd ipconfig'
+alias netstat='wincmd netstat'
+alias netsh='wincmd netsh'
+alias cscript='wincmd cscript'
+
+alias ping='wincmd /c/Windows/system32/PING.EXE'
